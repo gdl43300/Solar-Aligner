2dd709d25500576424241477d408983dd7ab7de7
guillaume <gdl43300@gmail.com>
2020-02-02 21:16:44 +0100

    Align function

--- Solar Aligner/Program.cs
+++ Solar Aligner/Program.cs
@@ -27,6 +27,9 @@ namespace IngameScript
         string oxygenFarmsGroup = "Ciapa-Oxygen Farms";
         string rotorsGroup = "Ciapa-Solar Rotors";
 
+        float searchSpeed = 0.1f;
+        float alignSpeed = 0.05f;
+
 
 
         List<IMySolarPanel> solarPanels;
@@ -36,61 +39,55 @@ namespace IngameScript
         float oxygenProducedLast;
         float energyProduced = 0;
         float energyProducedLast;
+        IEnumerator<bool> state;
 
         public Program()
         {
 
-            //Runtime.UpdateFrequency = UpdateFrequency.Update100;
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
 
             solarPanels = new List<IMySolarPanel>();
             rotors = new List<IMyMotorStator>();
             oxygenFarms = new List<IMyOxygenFarm>();
 
-            initLists();
+            InitLists();
             ProductionCalcul();
-
+            state = AlignRotors();
         }
 
         public void Save()
         {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
-
+            
         }
 
         public void Main(string argument, UpdateType updateSource)
         {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked,
-            // or the script updates itself. The updateSource argument
-            // describes where the update came from. Be aware that the
-            // updateSource is a  bitfield  and might contain more than 
-            // one update type.
-            // 
-            // The method itself is required, but the arguments above
-            // can be removed if not needed.
-            printLists();
+            
+            ProductionCalcul();
+            PrintLists();
+            if (!state.MoveNext())
+            {
+                state.Dispose();
+                state = AlignRotors();
+            }
+
         }
 
-        public void printLists()
+        public void PrintLists()
         {
-            Echo("Rotors : " + rotors.Count.ToString());
+            Echo("Rotors(" + rotors.Count +  ") : ");
             foreach (var rotor in rotors)
             {
                 Echo(rotor.DisplayNameText);
             }
 
-            Echo("\nSolar Panels : " + solarPanels.Count.ToString());
+            Echo("\nSolar Panels (" + solarPanels.Count + ") : " + energyProduced + "/" + energyProducedLast + " MW");
             foreach (var solar in solarPanels)
             {
                 Echo(solar.DisplayNameText + " : " + solar.MaxOutput + "MW");
             }
 
-            Echo("\nOxygen Farms : " + oxygenFarms.Count.ToString());
+            Echo("\nOxygen Farms (" + oxygenFarms.Count + ") : " + oxygenProduced + "/" + oxygenProducedLast + " L");
             foreach (var oxy in oxygenFarms)
             {
                 Echo(oxy.DisplayNameText + " : " + oxy.GetOutput() + " L");
@@ -98,7 +95,7 @@ namespace IngameScript
 
         }
 
-        public void initLists()
+        public void InitLists()
         {
             IMyBlockGroup temp = GridTerminalSystem.GetBlockGroupWithName(solarPanelsGroup);
             if (temp != null)
@@ -129,7 +126,7 @@ namespace IngameScript
             }
         }
 
-        public void checkLists()
+        public void CheckLists()
         {
             bool majNedded = false;
 
@@ -139,7 +136,7 @@ namespace IngameScript
 
             if (majNedded)
             {
-                initLists();
+                InitLists();
             }
         }
 
@@ -159,6 +156,61 @@ namespace IngameScript
                 oxygenProduced += oxy.GetOutput();
             }
 
+        }
+
+        public IEnumerator<bool> AlignRotors()
+        {
+            foreach(var rotor in rotors)
+            {
+                float angleOpti = rotor.Angle;
+                rotor.TargetVelocityRPM = searchSpeed;
+                yield return true;
+                while (GetDifference() > 0)
+                {
+                    angleOpti = rotor.Angle;
+                    yield return true;
+                }
+                rotor.TargetVelocityRPM = -searchSpeed;
+                yield return true;
+                while (GetDifference() > 0)
+                {
+                    angleOpti = rotor.Angle;
+                    yield return true;
+                }
+
+                while (SetAngle(rotor, angleOpti))
+                {
+                    yield return true;
+                }
+
+
+            }
+            yield return false;
+        }
+
+        private bool SetAngle(IMyMotorStator rotor, float angleOpti)
+        {
+            float delta = 0.5f;
+            float dif = angleOpti - rotor.Angle;
+            if (Math.Abs(dif) < delta)
+            {
+                rotor.TargetVelocityRPM = 0;
+                return false;
+            }
+            if (dif < 0)
+            {
+                rotor.TargetVelocityRPM = -alignSpeed;
+            }
+            else
+            {
+                rotor.TargetVelocityRPM = alignSpeed;
+            }
+            return true;
+        }
+
+        public float GetDifference()
+        {
+            return (energyProduced - energyProducedLast) / energyProduced + (oxygenProduced - oxygenProducedLast) / oxygenProduced;
         }
     }
 }
